bf921dbfbfc479beb99d732c44a2d353df909c4c
Nick Chemsak <nchemsak@gmail.com>
2016-11-16 11:49:12 -0600

    handlebars template

--- js/quiz.js
+++ js/quiz.js
@@ -1,47 +1,47 @@
 "use strict";
 
 let carInventory = require('./carLot'),
-    eventStuff = require('./events');
+  eventStuff = require('./events'),
 
-function populatePage (inventory) {
+  /////////////////////////////////////////////////////////////////////////////
+  //                PULL IN HANDLEBARS LIBRARY
+  /////////////////////////////////////////////////////////////////////////////
+  Handlebars = require('hbsfy/runtime'),
+
+  /////////////////////////////////////////////////////////////////////////////
+  //  CREATE A FOLDER CALLED TEMPLATES in the root of your project and car-grid.hbs file, add code below to link it
+  /////////////////////////////////////////////////////////////////////////////
+  carTemplate = require('../templates/car-grid.hbs');
+
+
+function populatePage(inventory) {
   // Grab the div we want to apppend the cards to
   let cards = document.getElementById("inventory-cards");
+  let cardDiv = document.createElement("div");
 
-  // Loop over the inventory and populate the page
-  inventory.forEach(function(car, index) {
-    let carCard = buildCard(car, index);
-    let cardDiv = document.createElement("div"); 
-    cardDiv.innerHTML = (carCard);
-    cards.appendChild(cardDiv);
-    let card = document.getElementById(`card--${index}`);
-  });
+  /////////////////////////////////////////////////////////////////////////////
+  //  push the .hbs file to the innerHTML
+  /////////////////////////////////////////////////////////////////////////////
+  cardDiv.innerHTML = (carTemplate(inventory));
 
-  // Now that the DOM is loaded, establish all the event listeners needed
-  eventStuff();
+  cards.appendChild(cardDiv);
+
+
+// Now that the DOM is loaded, establish all the event listeners needed
+eventStuff();
 }
 
-function buildCard (car, index) {
-  // Build a string that creates a single card
-  var card = "",
-      card_wrapper = `<section id="card--${index}" class="card-wrapper" style="border: 2px solid ${car.color}">`,
-      card_title = `<h3>${car.year} ${car.make} ${car.model}</h3>`,
-      card_price = `<div class="car-price">$${car.price}</div>`,
-      card_image = `<div class="card-img" style="background-image: url('images/${car.image}')"></div>`,
-      card_sold = car.purchased ? `<span class="card-sold">SOLD!</span>` : "",
-      card_copy = `<p class="card-copy">${car.description}</p>`;
-
-  // Concatenate our completed card markup
-  card += `${card_wrapper + card_title + card_price + card_image + card_sold + card_copy}</section>`;
-  return card;
-}
 
 // The Promises Way puts the callback responsibility on the caller
 carInventory.loadInventory()
-.then(
-  function (inventoryFromLoadInventoryResolve) {
-    populatePage(inventoryFromLoadInventoryResolve);
-    console.log("carPromise", inventoryFromLoadInventoryResolve);
-  },
-  function (reason) {
-    console.error('Something went wrong', reason);
-  });
+  .then(
+    function(inventoryFromLoadInventoryResolve) {
+      populatePage(inventoryFromLoadInventoryResolve);
+      console.log("carPromise", inventoryFromLoadInventoryResolve);
+      console.log({ inventoryFromLoadInventoryResolve });
+
+
+    },
+    function(reason) {
+      console.error('Something went wrong', reason);
+    });
